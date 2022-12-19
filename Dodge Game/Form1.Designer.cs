8459e4dd11fead8d084756598840d65d728b12bd
LogaGill489 <logagill489@ed.amdsb.ca>
2022-12-19 10:55:27 -0500

    Add project files.

---
+++ Dodge Game/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Dodge_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.topLabel = new System.Windows.Forms.Label();
+            this.escapeLabel = new System.Windows.Forms.Label();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.easyLabel = new System.Windows.Forms.Label();
+            this.mediumLabel = new System.Windows.Forms.Label();
+            this.hardLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 16;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // topLabel
+            // 
+            this.topLabel.BackColor = System.Drawing.Color.Transparent;
+            this.topLabel.Font = new System.Drawing.Font("Palatino Linotype", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLabel.ForeColor = System.Drawing.Color.MediumSpringGreen;
+            this.topLabel.Location = new System.Drawing.Point(175, 46);
+            this.topLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(218, 80);
+            this.topLabel.TabIndex = 3;
+            this.topLabel.Text = "You Win!";
+            this.topLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.topLabel.Visible = false;
+            // 
+            // escapeLabel
+            // 
+            this.escapeLabel.BackColor = System.Drawing.Color.Transparent;
+            this.escapeLabel.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.escapeLabel.ForeColor = System.Drawing.Color.Turquoise;
+            this.escapeLabel.Location = new System.Drawing.Point(312, 179);
+            this.escapeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.escapeLabel.Name = "escapeLabel";
+            this.escapeLabel.Size = new System.Drawing.Size(160, 29);
+            this.escapeLabel.TabIndex = 4;
+            this.escapeLabel.Text = "Press \"Escape\" to Exit";
+            this.escapeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // winLabel
+            // 
+            this.winLabel.BackColor = System.Drawing.Color.Transparent;
+            this.winLabel.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winLabel.ForeColor = System.Drawing.Color.Aquamarine;
+            this.winLabel.Location = new System.Drawing.Point(160, 115);
+            this.winLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(258, 54);
+            this.winLabel.TabIndex = 5;
+            this.winLabel.Text = "You Took (Time) Seconds to Win on (Difficulty) Mode!";
+            this.winLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.winLabel.Visible = false;
+            // 
+            // easyLabel
+            // 
+            this.easyLabel.BackColor = System.Drawing.Color.Transparent;
+            this.easyLabel.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.easyLabel.ForeColor = System.Drawing.Color.Turquoise;
+            this.easyLabel.Location = new System.Drawing.Point(75, 148);
+            this.easyLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.easyLabel.Name = "easyLabel";
+            this.easyLabel.Size = new System.Drawing.Size(201, 32);
+            this.easyLabel.TabIndex = 6;
+            this.easyLabel.Text = "Press \"E\" for Easy Difficulty";
+            this.easyLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mediumLabel
+            // 
+            this.mediumLabel.BackColor = System.Drawing.Color.Transparent;
+            this.mediumLabel.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediumLabel.ForeColor = System.Drawing.Color.Turquoise;
+            this.mediumLabel.Location = new System.Drawing.Point(58, 180);
+            this.mediumLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.mediumLabel.Name = "mediumLabel";
+            this.mediumLabel.Size = new System.Drawing.Size(218, 28);
+            this.mediumLabel.TabIndex = 7;
+            this.mediumLabel.Text = "Press \"M\" for Medium Difficulty";
+            this.mediumLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // hardLabel
+            // 
+            this.hardLabel.BackColor = System.Drawing.Color.Transparent;
+            this.hardLabel.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardLabel.ForeColor = System.Drawing.Color.Turquoise;
+            this.hardLabel.Location = new System.Drawing.Point(291, 147);
+            this.hardLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.hardLabel.Name = "hardLabel";
+            this.hardLabel.Size = new System.Drawing.Size(209, 32);
+            this.hardLabel.TabIndex = 8;
+            this.hardLabel.Text = "Press \"H\" for Hard Difficulty";
+            this.hardLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(586, 368);
+            this.Controls.Add(this.hardLabel);
+            this.Controls.Add(this.mediumLabel);
+            this.Controls.Add(this.easyLabel);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.escapeLabel);
+            this.Controls.Add(this.topLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Catch Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.Label escapeLabel;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Label easyLabel;
+        private System.Windows.Forms.Label mediumLabel;
+        private System.Windows.Forms.Label hardLabel;
+    }
+}
+
---
+++ Dodge Game/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace Dodge_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random randGen = new Random();
+
+        List <Rectangle> balls = new List<Rectangle>();
+
+        List <int> ballWidthSizes = new List<int>();
+        List<int> ballLengthSizes = new List<int>();
+        List <int> ballSpeeds = new List<int>();
+
+        List <string> shapes = new List<string>();
+
+        Rectangle player = new Rectangle(20, 177, 20, 20);
+
+        Brush whiteBrush = new SolidBrush(Color.White);
+        Brush grayBrush = new SolidBrush(Color.Gray);
+        Brush orangeBrush = new SolidBrush(Color.Orange);
+
+        Pen redPen = new Pen(Color.OrangeRed, 3);
+        Pen bluePen = new Pen(Color.Aquamarine, 3);
+
+        bool wDown = false;
+        bool sDown = false;
+        bool aDown = false;
+        bool dDown = false;
+
+        int playerSpeed = 7;
+
+        int randValue;
+
+        string difficulty = "Easy";
+
+        Stopwatch gameTime = new Stopwatch();
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.E:
+                    difficulty = "Easy";
+                    startGame();
+                    break;
+                case Keys.M:
+                    startGame();
+                    difficulty = "Medium";
+                    break;
+                case Keys.H:
+                    startGame();
+                    difficulty = "Hard";
+                    break;
+
+                case Keys.Escape:
+                    if (gameTimer.Enabled == false)
+                    {
+                        Application.Exit();
+                    }
+                        break;
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+            }
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(orangeBrush, player);
+
+            if (gameTimer.Enabled == false)
+            {
+                e.Graphics.FillRectangle(whiteBrush, 47, 47, 487, 267);
+                e.Graphics.FillRectangle(grayBrush, 50, 50, 480, 260);
+
+                e.Graphics.DrawLine(redPen, 60, 90, 90, 60);
+                e.Graphics.DrawLine(redPen, 60, 110, 110, 60);
+
+                e.Graphics.DrawLine(bluePen, 520, 270, 490, 300);
+                e.Graphics.DrawLine(bluePen, 520, 250, 470, 300);
+            }
+
+            for (int i = 0; i < balls.Count; i++)
+            {
+                if (shapes[i] == "rectangle")
+                {
+                    e.Graphics.FillRectangle(whiteBrush, balls[i]);
+                }
+                else if (shapes[i] == "ellipse")
+                {
+                    e.Graphics.FillEllipse(whiteBrush, balls[i]);
+                }
+            }
+        }
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (wDown == true && player.Y > 2)
+            {
+                player.Y -= playerSpeed;
+            }
+            if (sDown == true && player.Y < this.Height - 23)
+            {
+                player.Y += playerSpeed;
+            }
+            if (aDown == true && player.X > 2)
+            {
+                player.X -= playerSpeed;
+            }
+            if (dDown == true && player.X < this.Width - 23)
+            {
+                player.X += playerSpeed;
+            }
+
+            //move ball objects
+            for (int i = 0; i < balls.Count(); i++)
+            {
+                int y = balls[i].Y + ballSpeeds[i];
+                balls[i] = new Rectangle(balls[i].X, y, ballWidthSizes[i], ballLengthSizes[i]);
+
+                //remove balls at bottom of screen
+                if (balls[i].Y >= this.Height)
+                {
+                    balls.RemoveAt(i);
+                    ballWidthSizes.RemoveAt(i);
+                    ballLengthSizes.RemoveAt(i);
+                    ballSpeeds.RemoveAt(i);
+                    shapes.RemoveAt(i);
+                }
+
+                //check if balls have player collision
+                if (balls[i].IntersectsWith(player))
+                {
+                    player.Y = 177;
+                    player.X = 20;
+                }
+            }
+
+            //generate a random value
+            if (difficulty == "Easy")
+            {
+                randValue = randGen.Next(1, 151);
+            }
+            else if (difficulty == "Medium")
+            {
+                randValue = randGen.Next(1, 91);
+            }
+            else if (difficulty == "Hard")
+            {
+                randValue = randGen.Next(1, 71);
+            }
+
+            //generate new ball if it is time
+            if (randValue < 40)
+            {
+                balls.Add(new Rectangle(randGen.Next(40, this.Width - 60), 0, 0, 0));
+                ballWidthSizes.Add(randGen.Next(7, 20));
+                ballLengthSizes.Add(randGen.Next(7, 20));
+                ballSpeeds.Add(randGen.Next(6, 12));
+
+                randValue = randGen.Next(1, 20);
+
+                if (randValue < 10)
+                {
+                    shapes.Add("rectangle");
+                }
+                else
+                {
+                    shapes.Add("ellipse");
+                }
+            }
+
+            if (player.X >= this.Width - player.Width)
+            {
+                gameTimer.Enabled = false;
+
+                balls.Clear();
+                ballSpeeds.Clear();
+                ballWidthSizes.Clear();
+                ballLengthSizes.Clear();
+                shapes.Clear();
+
+                easyLabel.Visible = true;
+                mediumLabel.Visible = true;
+                hardLabel.Visible = true;
+
+                topLabel.Visible = true;
+                winLabel.Visible = true;
+                escapeLabel.Visible = true;
+
+                player.X = 20;
+                player.Y = 177;
+
+                gameTime.Stop();
+                long time = gameTime.ElapsedMilliseconds;
+                winLabel.Text = $"You took {gameTime.Elapsed.ToString(@"s\.fff")} Seconds to Win on {difficulty} Mode!";
+                gameTime.Reset();
+
+                this.Focus();
+            }
+
+            Refresh();
+        }
+        public void startGame()
+        {
+            if (gameTimer.Enabled == false)
+            {
+                gameTimer.Enabled = true;
+
+                easyLabel.Visible = false;
+                mediumLabel.Visible = false;
+                hardLabel.Visible = false;
+
+                topLabel.Visible = false;
+                winLabel.Visible = false;
+                escapeLabel.Visible = false;
+
+                easyLabel.Location = new Point(easyLabel.Location.X, 180);
+                mediumLabel.Location = new Point(mediumLabel.Location.X, 214);
+
+                hardLabel.Location = new Point(hardLabel.Location.X, 180);
+                escapeLabel.Location = new Point(escapeLabel.Location.X, 210);
+
+                gameTime.Start();
+            }
+        }
+    }
+}
